d3d99d3188cb71ea09cb3ec95f476821e5fa07bf
Charles Torry <ctorry@outlook.com>
2021-03-20 10:33:14 -0400

    Reached end of 110

--- lib/widgets/new_transation.dart
+++ lib/widgets/new_transation.dart
@@ -51,13 +51,27 @@ class _NewTransactionState extends State<NewTransaction> {
               // onChanged: (val) => amountInput = val,
               onSubmitted: (_) => submitData(),
             ),
-            TextButton(
+            Container(
+              height: 70,
+              child: Row(
+                children: [
+                  Text('No Date Chosen!'),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            ElevatedButton(
               onPressed: submitData,
               child: Text(
                 'Add Transaction',
-              ),
-              style: TextButton.styleFrom(
-                primary: Colors.purple,
               ),
             )
           ],
